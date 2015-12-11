547e7e1d9a86fec989c70dcd5780aaf4e03a4cb5
Kevin Hamacher <kevin.hamacher@rub.de>
2015-12-12 00:07:44 +0100

    Dwuid did bad

--- scenarios/01_Punkt_Feature_WHOIS/check_whois.py
+++ scenarios/01_Punkt_Feature_WHOIS/check_whois.py
@@ -1,64 +1,58 @@
 #!/usr/bin/env python3
-import socket
-import string
+"""Check if you have correctly implemented the WHOIS feature
+Call like this:
+    $ ./check_whois.py
+Or, if you want to change the port and host:
+    $ ./check_whois.py PORT HOST
+"""
 import sys
-import random
-import re
 
 
-# you may give this script another port and host to check over argv like this:
-#     ./check_whois.py 9999 otherhost.com
+# close your eyes - if you don't look it does not look hacky :-)
+sys.path.append('../../implementation/checkers')
+import checker
+
+
 PORT = 6667 if len(sys.argv) < 2 else sys.argv[1]
 HOST = 'localhost' if len(sys.argv) < 3 else sys.argv[2]
 
 
-def generate_string(length):
-    return ''.join(random.choice(string.ascii_letters) for _ in range(length))
+client1 = checker.RandomClient(HOST, PORT)
+client2 = checker.RandomClient(HOST, PORT)
 
 
-class RandomClient:
-    def __init__(self):
-        self.nickname = generate_string(10)
-        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._socket.settimeout(5)
-        self._socket.connect((HOST, PORT))
-        self.send('NICK :%s' % self.nickname)
-
-    def send(self, cmd):
-        self._socket.send(cmd.encode() + b'\n')
-
-    def recv_until(self, text, error):
-        data = ''
-        try:
-            while text not in data:
-                data += self._socket.recv(1024).decode()
-        except socket.timeout:
-            raise Exception(error)
-        return data
-
-
-client1 = RandomClient()
-client2 = RandomClient()
-
-
-channel = '#' + generate_string(10)
+channel = '#' + checker.generate_string(10)
 client1.send('JOIN :%s' % channel)
 
 
 client2.send('WHOIS :%s' % client1.nickname)
-replies = client2.recv_until(' 318 ',
-                             error='Waited for 5 seconds on 318 status code.')
+error_msg = 'Waited for 5 seconds on 318 status code.'
+replies = client2.recv_until(' 318 ', error=error_msg)
 
 
-assert re.search(r':[^ ]+ 311 %s %s x [^ ]+ \* :[\da-fA-F:\.]+:\d+'
-                 % (client2.nickname, client1.nickname),
-                 replies), \
-       'Status 311 message missing/incomplete'
-assert re.search(r':[^ ]+ 319 %s %s :%s'
-                 % (client2.nickname, client1.nickname, channel),
-                 replies), \
-       'Status 319 message mssing/incomplete'
-assert re.search(r':[^ ]+ 318 %s %s :End of /WHOIS list\.'
-                 % (client2.nickname, client1.nickname),
-                 replies), \
-       'Status 318 message incomplete'
+checker.check_regex(r':[^ ]+ 311 %s %s x [^ ]+ \* :[\da-fA-F:\.]+:\d+'
+                    % (client2.nickname, client1.nickname),
+                    replies,
+                    'Status 311 message missing/incomplete')
+
+
+checker.check_regex(r':[^ ]+ 319 %s %s :%s'
+                    % (client2.nickname, client1.nickname, channel),
+                    replies,
+                    'Status 319 message mssing/incomplete')
+
+
+checker.check_regex(r':[^ ]+ 318 %s %s :End of /WHOIS list\.'
+                    % (client2.nickname, client1.nickname),
+                    replies,
+                    'Status 318 message incomplete')
+
+
+unconnected_nick = checker.generate_string(20)
+client2.send('WHOIS :%s' % unconnected_nick)
+replies = client2.recv_until(' 401 ',
+                             error='Waited for 5 seconds on 401 status code.')
+checker.check_regex(r':[^ ]+ 401 %s %s :No such nick/channel'
+                    % (client2.nickname, unconnected_nick),
+                    replies,
+                    'Status 401 message incomplete')
